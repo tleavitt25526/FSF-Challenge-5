6e8421644692780c6a0e4e3d0cd62df019446fa1
tleavitt25526 <tleavitt25526@gmail.com>
2024-09-02 16:21:34 -0400

    Forgot to switch to branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,5 +36,5 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    
 });
