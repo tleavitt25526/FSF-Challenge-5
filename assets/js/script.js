edae5473623870ac03ec2e287800f11172389cb0
tleavitt25526 <tleavitt25526@gmail.com>
2024-09-03 11:50:43 -0400

    Importing dayjs the correct way without node

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,22 @@ const btn = document.getElementById('taskBtn');
 const submitBtn = document.getElementById('submitBtn');
 const span = document.getElementsByClassName('close')[0];
 
+// task elements
+const titleIn = document.getElementById('title');
+const dateIn = document.getElementById('date');
+const descIn = document.getElementById('desc');
+
+// dayjs object
+const now = dayjs()
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
+    // a bit clunky
+    nextId = JSON.parse(localStorage.getItem("nextId"));
     let id = nextId;
-    JSON.stringify(nextId + 1);
+    if (id == null) id = 0;
+    JSON.stringify(nextId);
+    localStorage.setItem("nextId", JSON.parse(id + 1));
     return id;
 }
 
@@ -46,6 +58,20 @@ $(document).ready(function () {
         modal.style.display = 'block'
     }
     submitBtn.onclick = function() {
+        // create task object and send to local storage
+        const task = {
+            title: titleIn.value,
+            data: dateIn.value,
+            desc: dateIn.value,
+            id: generateTaskId()
+        }
+        localStorage.setItem(task.id, JSON.stringify(task));
+
+        // clear modal content
+        titleIn.value = '';
+        dateIn.value = '';
+        descIn.value = '';
+
         modal.style.display = 'none';
     }
     span.onclick = function() {
